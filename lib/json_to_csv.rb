fb032f9b8a60c404d638dbf2b3634cc6d8fc0dae
youenn priser <youenn.priser@gmail.com>
2021-01-31 11:47:52 +0100

    Create the class Convert instanciated with the json filepath and calling every methods to compute that JSON and write that data into a CSV file

--- lib/json_to_csv.rb
+++ lib/json_to_csv.rb
@@ -6,65 +6,81 @@ module JsonToCsv
   class Error < StandardError; end
   # Your code goes here...
 
-  # 1 - Read and parse the JSON file
-  def read_and_parse_json_file
-    response = File.read(@filepath)
-    @path = File.basename(@filepath, ".*")
-    @parsed_json = JSON.parse(response)
-  end
-  # 2 - Retrieve and dig JSON object keys to build headers (Object have the same schema)
-  def retrieve_object_keys(object, path = nil)
-    response = object.map do |key, value|
-      if value.is_a?(Hash)
-        full_path = path ? "#{path}.#{key}" : key
-        retrieve_object_keys(value, full_path)
-      else
-        if path && !path.nil?
-          "#{path}.#{key}"
+  class Convert
+    def initialize(filepath)
+      @filepath = filepath
+      convert
+    end
+
+    private
+
+    def convert
+      read_and_parse_json_file
+      build_headers
+      generate_csv_filepath
+      write_into_csv_file
+    end
+
+    def read_and_parse_json_file
+      response = File.read(@filepath)
+      @path = File.basename(@filepath, ".*")
+      @parsed_json = JSON.parse(response)
+    end
+
+    def build_headers
+      object = @parsed_json.first
+      @headers = retrieve_object_keys(object)
+    end
+
+    def retrieve_object_keys(object, path = nil)
+      response = object.map do |key, value|
+        if value.is_a?(Hash)
+          full_path = path ? "#{path}.#{key}" : key
+          retrieve_object_keys(value, full_path)
         else
-          key
+          if path && !path.nil?
+            "#{path}.#{key}"
+          else
+            key
+          end
+        end
+      end
+      response.compact.flatten
+    end
+
+    def generate_csv_filepath
+      @csv_filepath = "#{@path}.csv"
+    end
+
+    def write_into_csv_file
+      csv_options = { col_sep: ',', quote_char: '"' }
+      CSV.open(@csv_filepath, 'w+', csv_options) do |csv|
+        csv << @headers
+        @parsed_json.each do |object|
+          csv << build_object_csv_row(object)
         end
       end
     end
-    response.compact.flatten
-  end
-  # 3 - Header acts as path to lookup into object and retrieve value (specific computation for array values)
-  def retrieve_value(object, header)
-    path_keys = header.split('.')
-    path_keys.each do |path_key|
-      return nil if object.nil?
-      object = object[path_key]
-    end
-    value = compute_value(object)
-  end
 
-  # - Specific computation of Array values
-  def compute_value(value)
-    return value unless value.is_a?(Array)
-
-    value.join(",")
-  end
-
-  # 4 - Build the row to be inserted into the CSV file
-  def build_object_csv_row(object)
-    values = @headers.map do |path|
-      retrieve_value(object, path)
-    end
-  end
-
-  # 5 - Create the receiving CSV file
-  def generate_csv_filepath
-    @csv_filepath = "#{@path}.csv"
-  end
-
-  # 6 - Write into that CSV file
-  def write_into_csv_file
-    csv_options = { col_sep: ',', quote_char: '"' }
-    CSV.open(@csv_filepath, 'w+', csv_options) do |csv|
-      csv << @headers
-      @parsed_json.each do |object|
-        csv << build_object_csv_row(object)
+    def build_object_csv_row(object)
+      values = @headers.map do |path|
+        retrieve_value(object, path)
       end
+    end
+
+    def retrieve_value(object, header)
+      path_keys = header.split('.')
+      path_keys.each do |path_key|
+        return nil if object.nil?
+        object = object[path_key]
+      end
+      value = compute_value(object)
+    end
+
+    def compute_value(value)
+      return value unless value.is_a?(Array)
+
+      value.join(",")
     end
   end
 end
